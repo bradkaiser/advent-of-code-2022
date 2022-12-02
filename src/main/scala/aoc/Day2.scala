6418dbbd2c0198767e49bb0459fbc82458ff156a
Brad Kaiser <bradley.kaiser@banno.com>
2022-12-02 16:22:23 -0500

    cleanup

--- src/main/scala/aoc/Day2.scala
+++ src/main/scala/aoc/Day2.scala
@@ -1,7 +1,5 @@
 package aoc
 
-import aoc.Day2.Hand.vs
-
 object Day2 {
 
   sealed abstract class Hand(val score: Int)
@@ -9,21 +7,19 @@ object Day2 {
   case object Paper extends Hand(2)
   case object Scissors extends Hand(3)
 
-  object Hand {
-    def parse(c: Char): Hand = c match {
-      case 'A' | 'X' => Rock
-      case 'B' | 'Y' => Paper
-      case 'C' | 'Z' => Scissors
-    }
-
-    def vs(yours: Hand, theirs: Hand) = (yours, theirs) match {
-      case (Rock, Paper) | (Paper, Scissors) | (Scissors, Rock) => 0
-      case (Rock, Rock) | (Paper, Paper) | (Scissors, Scissors) => 3
-      case (Rock, Scissors) | (Paper, Rock) | (Scissors, Paper) => 6
-    }
+  def hand(c: Char): Hand = c match {
+    case 'A' | 'X' => Rock
+    case 'B' | 'Y' => Paper
+    case 'C' | 'Z' => Scissors
   }
 
-  def parseLine(s: String) = (Hand.parse(s.charAt(0)), Hand.parse(s.charAt(2)))
+  def vs(yours: Hand, theirs: Hand): Int = (yours, theirs) match {
+    case (Rock, Paper) | (Paper, Scissors) | (Scissors, Rock) => 0
+    case (Rock, Rock) | (Paper, Paper) | (Scissors, Scissors) => 3
+    case (Rock, Scissors) | (Paper, Rock) | (Scissors, Paper) => 6
+  }
+
+  def parseLine(s: String): (Hand, Hand) = (hand(s.charAt(0)), hand(s.charAt(2)))
   def score(theirs: Hand, yours: Hand): Int = vs(yours, theirs) + yours.score
-  def scoreAll(lines: List[String]) = lines.map(parseLine).map((score _).tupled).sum
+  def scoreAll(lines: List[String]): Int = lines.map(parseLine).map((score _).tupled).sum
 }
